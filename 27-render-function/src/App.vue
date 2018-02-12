2a4925fa4377053e7baf71d1a74e3235cb047083
Julian Caro <jcaro@doonamis.es>
2018-02-12 18:55:25 +0100

    Render function and hot reloading

---
+++ 27-render-function/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <elemento tipo="section">Hola mundo</elemento>
+    <elemento tipo="header">Hola header</elemento>
+    <elemento tipo="footer">Hola footer</elemento>
+    <elemento tipo="aside">Hola aside</elemento>
+  </div>
+</template>
+
+<script>
+  import elemento from './components/Elemento.vue';
+  export default {
+    components: {elemento}
+  }
+</script>
+
+<style>
+
+</style>
---
+++ 28-hot-module-reload/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <tareas></tareas>
+</template>
+
+<script>
+  import Tareas from './components/Tareas.vue';
+  export default {
+    components: {Tareas}
+  }
+</script>
+
+<style>
+</style>
