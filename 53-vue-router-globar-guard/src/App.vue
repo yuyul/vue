701b9be83b68578df6e38e01285b4d7a4f1b47d8
Julian Caro <jcaro@doonamis.es>
2018-03-01 18:21:54 +0100

    vue-router-global-guard

---
+++ 53-vue-router-globar-guard/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <button @click="cambiar">Cambiar Auth</button>
+    <hr>
+    <router-view></router-view>
+  </section>
+  
+</template>
+
+<script>
+import {mapMutations} from 'Vuex';
+export default {
+  methods: mapMutations(['cambiar']),
+}
+</script>
+
