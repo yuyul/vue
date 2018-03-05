be0b4334cc70e208af3faa11055e77e9efc5c734
Julian Caro <jcaro@doonamis.es>
2018-03-05 18:28:26 +0100

    Vue router route guard

---
+++ 54-vue-router-route-guard/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>Home</h1>
+    <!-- <router-link :to="'/equipo/' + id">Equipo</router-link> -->
+    <router-link :to="{ name: 'equipo', params: { id: id }}">Equipo</router-link>
+    <router-link :to="{ name: 'contacto' }">Contacto</router-link>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        id: 'juan'
+      }
+    }
+  }
+</script>
